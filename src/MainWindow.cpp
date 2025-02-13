5a3f9087ada0a5020b73d4b7b7d9f68df0401623
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-02-14 00:03:51 +0800

    InputWidget

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,34 +1,6 @@
 #include "MainWindow.h"
 #include <QLineEdit>
-
-class CustomWidget :
-	public QWidget
-{
-public:
-	CustomWidget(QWidget *parent = nullptr)
-		: QWidget(parent)
-	{
-		// 创建水平布局
-		QHBoxLayout *layout = new QHBoxLayout(this);
-
-		// 创建输入框
-		QLineEdit *lineEdit = new QLineEdit(this);
-		lineEdit->setPlaceholderText("Enter text here...");
-		lineEdit->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred); // 输入框自适应缩放
-
-		// 创建按钮
-		QPushButton *button = new QPushButton("Submit", this);
-		button->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Preferred); // 按钮大小由内容决定
-
-		// 将控件添加到布局中
-		layout->addWidget(lineEdit);
-		layout->addWidget(button);
-
-		// 设置布局的间距和边距
-		layout->setSpacing(10);                     // 控件之间的间距
-		layout->setContentsMargins(10, 10, 10, 10); // 布局与窗口边缘的距离
-	}
-};
+#include <widget/InputWidget.h>
 
 widget::MainWindow::MainWindow()
 {
@@ -53,7 +25,7 @@ widget::MainWindow::MainWindow()
 	QPushButton *button3 = new QPushButton{"按钮 3", centralWidget};
 	layout->addWidget(button3);
 
-	CustomWidget *w = new CustomWidget{centralWidget};
+	widget::InputWidget *w = new widget::InputWidget{centralWidget};
 	layout->addWidget(w);
 
 	connect(button1,
@@ -61,5 +33,12 @@ widget::MainWindow::MainWindow()
 			[this]()
 			{
 				close();
+			});
+
+	connect(button2,
+			&QPushButton::clicked,
+			[this, button1]()
+			{
+				button1->clicked();
 			});
 }
