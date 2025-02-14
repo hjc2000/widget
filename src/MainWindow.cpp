0b985530e130894920ab94d135cce9df9e0a8210
hjc2000 <hjc.666@foxmail.com>
2025-02-14 14:17:17 +0800

    QTableView

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,78 +1,110 @@
 #include "MainWindow.h"
 #include <base/string/Parse.h>
 #include <QAbstractTableModel>
+#include <QHeaderView>
 #include <QLineEdit>
+#include <QPainter>
+#include <QStyledItemDelegate>
+#include <QTableView>
 #include <string>
 #include <widget/Conversion.h>
 #include <widget/InputWidget.h>
 
-// 自定义模型
-class MyTableModel :
-	public QAbstractTableModel
+namespace
 {
-	Q_OBJECT
-
-public:
-	int rowCount(QModelIndex const &parent = QModelIndex()) const override
+	class MyTableModel :
+		public QAbstractTableModel
 	{
-		return 4;
-	}
+	public:
+		MyTableModel(QWidget *parent = nullptr)
+			: QAbstractTableModel(parent)
+		{
+		}
 
-	int columnCount(QModelIndex const &parent = QModelIndex()) const override
-	{
-		return 3;
-	}
+		virtual int rowCount(QModelIndex const &parent = QModelIndex{}) const override
+		{
+			return 200;
+		}
 
-	QVariant data(QModelIndex const &index, int role = Qt::DisplayRole) const override
-	{
-		if (role == Qt::DisplayRole)
-			return QString("Cell (%1, %2)").arg(index.row()).arg(index.column());
-		return QVariant();
-	}
+		virtual int columnCount(QModelIndex const &parent = QModelIndex{}) const override
+		{
+			return 3;
+		}
 
-	QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override
-	{
-		if (role == Qt::DisplayRole && orientation == Qt::Horizontal)
-			return QString("Column %1").arg(section + 1);
-		return QAbstractTableModel::headerData(section, orientation, role);
-	}
-};
+		virtual QVariant data(QModelIndex const &index, int role = Qt::DisplayRole) const override
+		{
+			if (role == Qt::DisplayRole)
+			{
+				return QString("Cell (%1, %2)").arg(index.row()).arg(index.column());
+			}
 
-#include "MainWindow.moc"
+			return QVariant{};
+		}
+
+		virtual QVariant headerData(int section,
+									Qt::Orientation orientation,
+									int role = Qt::DisplayRole) const override
+		{
+			if (role == Qt::DisplayRole && orientation == Qt::Horizontal)
+			{
+				return QString("Column %1").arg(section + 1);
+			}
+
+			return QAbstractTableModel::headerData(section, orientation, role);
+		}
+	};
+} // namespace
 
 widget::MainWindow::MainWindow()
 {
-	setWindowTitle("带有侧边栏的 Hello World");
+	setWindowTitle("test");
 
 	// 创建一个中央部件
 	QWidget *centralWidget = new QWidget{this};
-
-	// 设置中央部件
 	setCentralWidget(centralWidget);
 
 	QHBoxLayout *layout = new QHBoxLayout{centralWidget};
 	layout->setAlignment(Qt::Alignment{Qt::AlignmentFlag::AlignTop | Qt::AlignmentFlag::AlignLeft});
 
-	// 添加一些按钮到布局中作为示例
-	QPushButton *button1 = new QPushButton{"按钮 1", centralWidget};
-	layout->addWidget(button1);
+	// 创建 QTableView 和模型
+	QTableView *tableView = new QTableView{centralWidget};
+	MyTableModel *model = new MyTableModel{};
+	tableView->setModel(model);
+	layout->addWidget(tableView);
 
-	QPushButton *button2 = new QPushButton{"按钮 2", centralWidget};
-	layout->addWidget(button2);
+	// 获取水平头视图并设置拉伸模式
+	QHeaderView *header = tableView->horizontalHeader();
+	header->setSectionResizeMode(QHeaderView::Stretch); // 均分宽度并拉伸
 
-	QPushButton *button3 = new QPushButton{"按钮 3", centralWidget};
-	layout->addWidget(button3);
+	tableView->setSelectionBehavior(QAbstractItemView::SelectItems);
+	tableView->setSelectionMode(QAbstractItemView::SingleSelection);
 
-	widget::InputWidget *w = new widget::InputWidget{centralWidget};
-	layout->addWidget(w);
+	// 设置样式表，将焦点矩形的颜色和背景颜色设置为蓝色
+	tableView->setStyleSheet("QTableView::item:focus {"
+							 "   background-color: blue;" // 设置焦点背景颜色为蓝色
+							 "   color: white;"           // 可选：设置文本颜色为白色以提高对比度
+							 "}");
 
-	w->SubmitEvent().Subscribe(
-		[this, w](QString const &text)
-		{
-			std::string str;
-			str << w->Text();
+	// // 添加一些按钮到布局中作为示例
+	// QPushButton *button1 = new QPushButton{"按钮 1", centralWidget};
+	// layout->addWidget(button1);
 
-			int64_t num = base::ParseInt64(str);
-			w->SetText(std::to_string(num + 1));
-		});
+	// QPushButton *button2 = new QPushButton{"按钮 2", centralWidget};
+	// layout->addWidget(button2);
+
+	// QPushButton *button3 = new QPushButton{"按钮 3", centralWidget};
+	// layout->addWidget(button3);
+
+	// widget::InputWidget *w = new widget::InputWidget{centralWidget};
+	// layout->addWidget(w);
+
+	// w->SubmitEvent().Subscribe(
+	// 	[this, w](QString const &text)
+	// 	{
+	// 		std::string str;
+	// 		str << w->Text();
+
+	// 		int64_t num = base::ParseInt64(str);
+	// 		w->SetText(std::to_string(num + 1));
+	// 	});
 }
