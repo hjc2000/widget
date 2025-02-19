6b6f61cc87d5c0b69c186fd51728970da4dd84e3
hjc2000 <hjc.666@foxmail.com>
2025-02-19 15:18:08 +0800

    注释

--- include/widget/Button.h
+++ include/widget/Button.h
@@ -12,12 +12,6 @@ namespace widget
 		public QPushButton
 	{
 	private:
-		base::Delegate<> _clicked_event;
-		base::Delegate<> _pressed_event;
-		base::Delegate<> _released_event;
-		base::Delegate<> _enter_event;
-		base::Delegate<> _leave_event;
-
 		/// @brief 构造函数中记录的原始样式。
 		QPalette _origin_palette;
 
@@ -29,22 +23,83 @@ namespace widget
 
 		void ConnectSignal();
 
+	private: // 事件
+		base::Delegate<> _clicked_event;
+		base::Delegate<> _pressed_event;
+		base::Delegate<> _released_event;
+		base::Delegate<> _enter_event;
+		base::Delegate<> _leave_event;
+
 		virtual void enterEvent(QEnterEvent *event) override;
 		virtual void leaveEvent(QEvent *event) override;
 
-	public:
+	public: // 生命周期
 		Button();
 		Button(QString const &text);
 
+	public: // 事件
+		/**
+		 * @brief 按钮点击事件。
+		 *
+		 * @return base::IEvent<>&
+		 */
 		base::IEvent<> &ClickedEvent();
+
+		/**
+		 * @brief 鼠标按钮按下，没有释放时触发的事件。
+		 *
+		 * @return base::IEvent<>&
+		 */
 		base::IEvent<> &PressedEvent();
+
+		/**
+		 * @brief 鼠标按钮从按下状态变成释放状态时触发的事件。
+		 *
+		 * @return base::IEvent<>&
+		 */
 		base::IEvent<> &ReleasedEvent();
+
+		/**
+		 * @brief 鼠标进入按钮时触发的事件。
+		 *
+		 * @return base::IEvent<>&
+		 */
 		base::IEvent<> &EnterEvent();
+
+		/**
+		 * @brief 鼠标离开按钮时触发的事件。
+		 *
+		 * @return base::IEvent<>&
+		 */
 		base::IEvent<> &LeaveEvent();
 
+	public: // Text
+		/**
+		 * @brief 按钮的文本。
+		 *
+		 * @return QString
+		 */
 		QString Text() const;
+
+		/**
+		 * @brief 设置按钮的文本。
+		 *
+		 * @param value
+		 */
 		void SetText(QString const &value);
+
+		/**
+		 * @brief 设置按钮的文本。
+		 *
+		 * @param value
+		 */
 		void SetText(std::string const &value);
+
+		/**
+		 * @brief 设置按钮的文本。
+		 *
+		 * @param value
+		 */
 		void SetText(char const *value);
 	};
 } // namespace widget
--- include/widget/RangeSubmit.h
+++ include/widget/RangeSubmit.h
@@ -7,6 +7,12 @@
 
 namespace widget
 {
+	/**
+	 * @brief 提交一个范围。
+	 *
+	 * @note 分成左右 2 个输入框，2 个输入框中间一条短横线，表示 从...到... ，右侧一个提交按钮，
+	 * 点击后就是确认提交。
+	 */
 	class RangeSubmit :
 		public QWidget
 	{
@@ -25,12 +31,40 @@ namespace widget
 		RangeSubmit();
 
 	public: // 属性
+		/**
+		 * @brief 左侧输入框的文本。
+		 *
+		 * @return QString
+		 */
 		QString LeftText() const;
+
+		/**
+		 * @brief 左侧输入框的文本。
+		 *
+		 * @return std::string
+		 */
 		std::string LeftTextStdString() const;
+
+		/**
+		 * @brief 右侧输入框的文本。
+		 *
+		 * @return QString
+		 */
 		QString RightText() const;
+
+		/**
+		 * @brief 右侧输入框的文本。
+		 *
+		 * @return std::string
+		 */
 		std::string RightTextStdString() const;
 
 	public: // 事件
+		/**
+		 * @brief 用户点击提交按钮后触发的事件。
+		 *
+		 * @return base::IEvent<>&
+		 */
 		base::IEvent<> &SubmitEvent();
 	};
 } // namespace widget
