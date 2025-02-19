c9a13b6c293874eeea83fb53cb5d069ca681f64d
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-02-20 00:09:21 +0800

    HBoxLayout

--- include/widget/layout/HBoxLayout.cpp
+++ include/widget/layout/HBoxLayout.cpp
@@ -3,11 +3,7 @@
 widget::HBoxLayout::HBoxLayout(QWidget *parent)
 {
 	_layout = std::shared_ptr<QHBoxLayout>{new QHBoxLayout{parent}};
-}
 
-void widget::HBoxLayout::AddWidget(QWidget *widget)
-{
-	_layout->addWidget(widget);
 	_layout->setAlignment(Qt::Alignment{Qt::AlignmentFlag::AlignTop | Qt::AlignmentFlag::AlignLeft});
 
 	// 控件之间的间距。
@@ -15,4 +11,9 @@ void widget::HBoxLayout::AddWidget(QWidget *widget)
 
 	// 布局内的内容区域与布局边框的间距。实际上相当于网页中的盒子内边距。
 	_layout->setContentsMargins(0, 0, 0, 0);
+}
+
+void widget::HBoxLayout::AddWidget(QWidget *widget)
+{
+	_layout->addWidget(widget);
 }
