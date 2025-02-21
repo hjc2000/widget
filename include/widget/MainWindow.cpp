aac0b8f7642879cc6042cded6cd3d21c51a56737
hjc2000 <hjc.666@foxmail.com>
2025-02-21 17:27:37 +0800

    MainWindow

--- include/widget/MainWindow.cpp
+++ include/widget/MainWindow.cpp
@@ -1,17 +1,10 @@
 #include "MainWindow.h"
 #include "layout/Padding.h"
-#include <iostream>
 
 widget::MainWindow::MainWindow()
 {
 	setWindowTitle("test");
-	setCentralWidget(&_box);
+	setCentralWidget(_vbox.get());
 
-	_box.SetPadding(widget::Padding{10});
-
-	_range_submit->SubmitEvent().Subscribe(
-		[]()
-		{
-			std::cout << "提交" << std::endl;
-		});
+	_vbox->SetPadding(widget::Padding{10});
 }
--- include/widget/MainWindow.h
+++ include/widget/MainWindow.h
@@ -8,6 +8,7 @@
 #include <QDateTimeEdit>
 #include <QMainWindow>
 #include <QVBoxLayout>
+#include <widget/box/HBox.h>
 #include <widget/box/VBox.h>
 #include <widget/layout/FormTableLayout.h>
 #include <widget/layout/GridLayout.h>
@@ -26,10 +27,54 @@ namespace widget
 		public QMainWindow
 	{
 	private:
-		std::shared_ptr<widget::RangeSubmit> _range_submit{new widget::RangeSubmit{}};
-		std::shared_ptr<QCheckBox> _check_box{new QCheckBox{"选择框"}};
-
-		widget::VBox _box{_range_submit, _check_box};
+		std::shared_ptr<widget::VBox> _vbox{
+			new widget::VBox{
+				std::shared_ptr<widget::HBox>{
+					new widget::HBox{
+						std::shared_ptr<widget::RangeSubmit>{
+							new widget::RangeSubmit{},
+						},
+						std::shared_ptr<QCheckBox>{
+							new QCheckBox{"选择框"},
+						},
+					},
+				},
+				std::shared_ptr<widget::HBox>{
+					new widget::HBox{
+						std::shared_ptr<widget::RangeSubmit>{
+							new widget::RangeSubmit{},
+						},
+						std::shared_ptr<QCheckBox>{
+							new QCheckBox{"选择框"},
+						},
+						std::shared_ptr<widget::VBox>{
+							new widget::VBox{
+								std::shared_ptr<widget::HBox>{
+									new widget::HBox{
+										std::shared_ptr<widget::RangeSubmit>{
+											new widget::RangeSubmit{},
+										},
+										std::shared_ptr<QCheckBox>{
+											new QCheckBox{"选择框"},
+										},
+									},
+								},
+								std::shared_ptr<widget::HBox>{
+									new widget::HBox{
+										std::shared_ptr<widget::RangeSubmit>{
+											new widget::RangeSubmit{},
+										},
+										std::shared_ptr<QCheckBox>{
+											new QCheckBox{"选择框"},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		};
 
 	public:
 		/**
