16030bd32019a49968a0b511ae8fb80ae01a0560
hjc2000 <hjc.666@foxmail.com>
2025-02-21 09:17:16 +0800

    CustomItemDelegate

--- private_src/CustomItemDelegate.cpp
+++ private_src/CustomItemDelegate.cpp
@@ -1,1 +1,51 @@
 #include "CustomItemDelegate.h"
+
+void widget::CustomItemDelegate::paint(QPainter *painter,
+									   QStyleOptionViewItem const &option,
+									   QModelIndex const &index) const
+{
+	// 创建一个新的 QStyleOptionViewItem 并初始化
+	QStyleOptionViewItem new_option = option;
+	initStyleOption(&new_option, index);
+
+	QString text = index.data(Qt::DisplayRole).toString();
+
+	QTextOption text_option{};
+
+	text_option.setAlignment(Qt::AlignmentFlag::AlignLeft |
+							 Qt::AlignmentFlag::AlignVCenter);
+
+	QRect text_rect = new_option.rect;
+	text_rect.adjust(_padding, 0, -_padding, 0);
+
+	if (new_option.state & QStyle::State_Selected)
+	{
+		if (new_option.state & QStyle::State_Active)
+		{
+			painter->fillRect(new_option.rect, QColor{204, 232, 255});
+			painter->drawText(text_rect, text, text_option);
+		}
+		else
+		{
+			painter->fillRect(new_option.rect, QColor{240, 240, 240});
+			painter->drawText(text_rect, text, text_option);
+		}
+	}
+	else
+	{
+		painter->fillRect(new_option.rect, Qt::GlobalColor::white);
+		painter->drawText(text_rect, text, text_option);
+	}
+}
+
+QSize widget::CustomItemDelegate::sizeHint(QStyleOptionViewItem const &option, QModelIndex const &index) const
+{
+	QSize size = QStyledItemDelegate::sizeHint(option, index);
+
+	// 获取文本宽度并加上左右内边距
+	QFontMetrics fm(option.font);
+	QString text = index.data(Qt::DisplayRole).toString();
+	int textWidth = fm.horizontalAdvance(text) + 2 * _padding;
+	size.setWidth(qMax(size.width(), textWidth));
+	return size;
+}
--- private_src/CustomItemDelegate.h
+++ private_src/CustomItemDelegate.h
@@ -17,52 +17,8 @@ namespace widget
 	public:
 		void paint(QPainter *painter,
 				   QStyleOptionViewItem const &option,
-				   QModelIndex const &index) const override
-		{
-			// 创建一个新的 QStyleOptionViewItem 并初始化
-			QStyleOptionViewItem new_option = option;
-			initStyleOption(&new_option, index);
+				   QModelIndex const &index) const override;
 
-			QString text = index.data(Qt::DisplayRole).toString();
-
-			QTextOption text_option{};
-
-			text_option.setAlignment(Qt::AlignmentFlag::AlignLeft |
-									 Qt::AlignmentFlag::AlignVCenter);
-
-			QRect text_rect = new_option.rect;
-			text_rect.adjust(_padding, 0, -_padding, 0);
-
-			if (new_option.state & QStyle::State_Selected)
-			{
-				if (new_option.state & QStyle::State_Active)
-				{
-					painter->fillRect(new_option.rect, QColor{204, 232, 255});
-					painter->drawText(text_rect, text, text_option);
-				}
-				else
-				{
-					painter->fillRect(new_option.rect, QColor{240, 240, 240});
-					painter->drawText(text_rect, text, text_option);
-				}
-			}
-			else
-			{
-				painter->fillRect(new_option.rect, Qt::GlobalColor::white);
-				painter->drawText(text_rect, text, text_option);
-			}
-		}
-
-		QSize sizeHint(QStyleOptionViewItem const &option, QModelIndex const &index) const override
-		{
-			QSize size = QStyledItemDelegate::sizeHint(option, index);
-
-			// 获取文本宽度并加上左右内边距
-			QFontMetrics fm(option.font);
-			QString text = index.data(Qt::DisplayRole).toString();
-			int textWidth = fm.horizontalAdvance(text) + 2 * _padding;
-			size.setWidth(qMax(size.width(), textWidth));
-			return size;
-		}
+		QSize sizeHint(QStyleOptionViewItem const &option, QModelIndex const &index) const override;
 	};
 } // namespace widget
