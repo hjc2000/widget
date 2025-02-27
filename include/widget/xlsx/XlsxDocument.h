a200a4f58154626194791eb52ac07099bf9c17ff
hjc2000 <hjc.666@foxmail.com>
2025-02-27 14:47:50 +0800

    XlsxDocument

--- include/widget/xlsx/XlsxDocument.h
+++ include/widget/xlsx/XlsxDocument.h
@@ -43,46 +43,159 @@ namespace widget
 		XlsxDocument(QString const &file_path);
 
 	public:
+		/**
+		 * @brief 写入一个单元格。
+		 *
+		 * @param row
+		 * @param column
+		 * @param content
+		 */
 		void Write(int row, int column, QString const &content);
 
+		/**
+		 * @brief 写入一个单元格。
+		 *
+		 * @param row
+		 * @param column
+		 * @param content
+		 */
 		void Write(int row, int column, std::string const &content);
 
+		/**
+		 * @brief 写入一个单元格。
+		 *
+		 * @param row
+		 * @param column
+		 * @param content
+		 */
 		void Write(int row, int column, char const *content);
 
+		/**
+		 * @brief 写入一个单元格。
+		 *
+		 * @param row
+		 * @param column
+		 * @param content
+		 * @param format
+		 */
 		void Write(int row, int column, QString const &content, QXlsx::Format const &format);
 
+		/**
+		 * @brief 写入一个单元格。
+		 *
+		 * @param row
+		 * @param column
+		 * @param content
+		 * @param format
+		 */
 		void Write(int row, int column, std::string const &content, QXlsx::Format const &format);
 
+		/**
+		 * @brief 写入一个单元格。
+		 *
+		 * @param row
+		 * @param column
+		 * @param content
+		 * @param format
+		 */
 		void Write(int row, int column, char const *content, QXlsx::Format const &format);
 
+		/**
+		 * @brief 写入一个表格数据模型。
+		 *
+		 * @param model
+		 */
 		void Write(widget::ITableDataModel const &model);
 
+		/**
+		 * @brief 写入一个表格数据模型。
+		 *
+		 * @param model
+		 * @param format
+		 */
 		void Write(widget::ITableDataModel const &model, QXlsx::Format const &format);
 
+		/**
+		 * @brief 写入一个表格数据模型。
+		 *
+		 * @param model
+		 * @param column_title_format
+		 * @param row_title_format
+		 * @param data_format
+		 */
 		void Write(widget::ITableDataModel const &model,
 				   QXlsx::Format const &column_title_format,
 				   QXlsx::Format const &row_title_format,
 				   QXlsx::Format const &data_format);
 
 	public:
+		/**
+		 * @brief 加载 xlsx 文件。
+		 *
+		 */
 		void Load() const;
 
+		/**
+		 * @brief 获取指定位置的单元格。
+		 *
+		 * @param row
+		 * @param column
+		 * @return std::shared_ptr<QXlsx::Cell>
+		 */
 		std::shared_ptr<QXlsx::Cell> GetCellAt(int row, int column) const;
 
+		/**
+		 * @brief 将指定单元格的内容作为字符串读出来。
+		 *
+		 * @param row
+		 * @param column
+		 * @return QString
+		 */
 		QString ReadCellAsString(int row, int column) const;
 
+		/**
+		 * @brief 将指定单元格的内容作为字符串读出来。
+		 *
+		 * @param row
+		 * @param column
+		 * @return std::string
+		 */
 		std::string ReadCellAsStdString(int row, int column) const;
 
 	public:
+		/**
+		 * @brief 获取当前工作表。
+		 *
+		 * @return QXlsx::Worksheet*
+		 */
 		QXlsx::Worksheet *CurrentWorksheet() const;
 
 	public:
+		/**
+		 * @brief 保存当前文档到已经绑定的 xlsx 文件。
+		 *
+		 */
 		void Save() const;
 
+		/**
+		 * @brief 将当前文档另存为指定路径的 xlsx 文件。
+		 *
+		 * @param file_path
+		 */
 		void SaveAsFile(QString const &file_path) const;
 
+		/**
+		 * @brief 将当前文档另存为指定路径的 xlsx 文件。
+		 *
+		 * @param file_path
+		 */
 		void SaveAsFile(std::string const &file_path) const;
 
+		/**
+		 * @brief 将当前文档另存为指定路径的 xlsx 文件。
+		 *
+		 * @param file_path
+		 */
 		void SaveAsFile(char const *file_path) const;
 	};
 } // namespace widget
