484dce1686bcdffa0243922baaf95c6e2d555491
hjc2000 <hjc.666@foxmail.com>
2025-03-17 16:54:58 +0800

    pch

--- private_src/pch.h
+++ private_src/pch.h
@@ -2,17 +2,9 @@
 // IWYU pragma: begin_keep
 // IWYU pragma: begin_exports
 
+#include <base/pch.h>
+
 #ifdef __cplusplus
-	#include <base/bit/AutoBitConverter.h>
-	#include <base/container/Array.h>
-	#include <base/delegate/Delegate.h>
-	#include <base/delegate/IEvent.h>
-	#include <base/stream/ReadOnlySpan.h>
-	#include <base/stream/Span.h>
-	#include <base/string/ToHexString.h>
-	#include <base/time/TimePointSinceEpoch.h>
-	#include <exception>
-	#include <iostream>
 	#include <QApplication>
 	#include <QDateTimeEdit>
 	#include <QDebug>
