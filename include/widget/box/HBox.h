99f456bd725e57452e75fb12c49e20df9a3bd094
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-03-18 00:52:36 +0800

    注释

--- include/widget/box/HBox.h
+++ include/widget/box/HBox.h
@@ -14,91 +14,56 @@ namespace widget
 		std::set<std::shared_ptr<QWidget>> _widget_set;
 
 	public:
-		// 构造函数
+		/* #region 构造函数 */
 
-		/**
-		 * @brief Construct a new HBox object
-		 *
-		 */
 		HBox() = default;
-
-		/**
-		 * @brief Construct a new HBox object
-		 *
-		 * @param widgets
-		 */
 		HBox(std::initializer_list<std::shared_ptr<QWidget>> const &widgets);
-
-		/**
-		 * @brief Construct a new HBox object
-		 *
-		 * @param widgets
-		 */
 		HBox(std::vector<std::shared_ptr<QWidget>> const &widgets);
-
-		/**
-		 * @brief Construct a new HBox object
-		 *
-		 * @param widgets
-		 * @param padding
-		 */
 		HBox(std::vector<std::shared_ptr<QWidget>> const &widgets, widget::Padding const &padding);
-
-		/**
-		 * @brief Construct a new HBox object
-		 *
-		 * @param widgets
-		 */
 		HBox(base::IEnumerable<std::shared_ptr<QWidget>> const &widgets);
 
-		/**
-		 * @brief Construct a new HBox object
-		 *
-		 * @tparam TCount
-		 * @param widgets
-		 */
 		template <size_t TCount>
 		HBox(std::array<std::shared_ptr<QWidget>, TCount> const &widgets)
 		{
 			AddWidget(widgets);
 		}
 
-	public:
+		/* #endregion */
+
 		using IBox::AddWidget;
 
-		/**
-		 * @brief 向盒子添加一个控件。
-		 *
-		 * @param widget
-		 */
+		///
+		/// @brief 向盒子添加一个控件。
+		///
+		/// @param widget
+		///
 		virtual void AddWidget(std::shared_ptr<QWidget> const &widget) override;
 
-		/**
-		 * @brief 将控件从盒子中移除。
-		 *
-		 * @param widget
-		 */
+		///
+		/// @brief 将控件从盒子中移除。
+		///
+		/// @param widget
+		///
 		virtual void RemoveWidget(std::shared_ptr<QWidget> const &widget) override;
 
-		/**
-		 * @brief 清空盒子中的所有控件。
-		 *
-		 */
+		///
+		/// @brief 清空盒子中的所有控件。
+		///
+		///
 		virtual void ClearWidget() override;
 
-	public:
-		/**
-		 * @brief 获取内边距。
-		 *
-		 * @return widget::Padding
-		 */
+		///
+		/// @brief 获取内边距。
+		///
+		/// @return widget::Padding
+		///
 		virtual widget::Padding Padding() const override;
 
-		/**
-		 * @brief 设置内边距。
-		 *
-		 * @param value
-		 */
+		///
+		/// @brief 设置内边距。
+		///
+		/// @param value
+		///
 		virtual void SetPadding(widget::Padding const &value) override;
 	};
 } // namespace widget
--- include/widget/box/VBox.h
+++ include/widget/box/VBox.h
@@ -6,10 +6,10 @@
 
 namespace widget
 {
-	/**
-	 * @brief 垂直布局的盒子。
-	 *
-	 */
+	///
+	/// @brief 垂直布局的盒子。
+	///
+	///
 	class VBox :
 		public widget::IBox
 	{
@@ -18,91 +18,56 @@ namespace widget
 		std::set<std::shared_ptr<QWidget>> _widget_set;
 
 	public:
-		// 构造函数
+		/* #region 构造函数 */
 
-		/**
-		 * @brief Construct a new VBox object
-		 *
-		 */
 		VBox() = default;
-
-		/**
-		 * @brief Construct a new VBox object
-		 *
-		 * @param widgets
-		 */
 		VBox(std::initializer_list<std::shared_ptr<QWidget>> const &widgets);
-
-		/**
-		 * @brief Construct a new VBox object
-		 *
-		 * @param widgets
-		 */
 		VBox(std::vector<std::shared_ptr<QWidget>> const &widgets);
-
-		/**
-		 * @brief Construct a new VBox object
-		 *
-		 * @param widgets
-		 * @param padding
-		 */
 		VBox(std::vector<std::shared_ptr<QWidget>> const &widgets, widget::Padding const &padding);
-
-		/**
-		 * @brief Construct a new VBox object
-		 *
-		 * @param widgets
-		 */
 		VBox(base::IEnumerable<std::shared_ptr<QWidget>> const &widgets);
 
-		/**
-		 * @brief Construct a new VBox object
-		 *
-		 * @tparam TCount
-		 * @param widgets
-		 */
 		template <size_t TCount>
 		VBox(std::array<std::shared_ptr<QWidget>, TCount> const &widgets)
 		{
 			AddWidget(widgets);
 		}
 
-	public:
+		/* #endregion */
+
 		using IBox::AddWidget;
 
-		/**
-		 * @brief 向盒子添加一个控件。
-		 *
-		 * @param widget
-		 */
+		///
+		/// @brief 向盒子添加一个控件。
+		///
+		/// @param widget
+		///
 		virtual void AddWidget(std::shared_ptr<QWidget> const &widget) override;
 
-		/**
-		 * @brief 将控件从盒子中移除。
-		 *
-		 * @param widget
-		 */
+		///
+		/// @brief 将控件从盒子中移除。
+		///
+		/// @param widget
+		///
 		virtual void RemoveWidget(std::shared_ptr<QWidget> const &widget) override;
 
-		/**
-		 * @brief 清空盒子中的所有控件。
-		 *
-		 */
+		///
+		/// @brief 清空盒子中的所有控件。
+		///
+		///
 		virtual void ClearWidget() override;
 
-	public:
-		/**
-		 * @brief 获取内边距。
-		 *
-		 * @return widget::Padding
-		 */
+		///
+		/// @brief 获取内边距。
+		///
+		/// @return widget::Padding
+		///
 		virtual widget::Padding Padding() const override;
 
-		/**
-		 * @brief 设置内边距。
-		 *
-		 * @param value
-		 */
+		///
+		/// @brief 设置内边距。
+		///
+		/// @param value
+		///
 		virtual void SetPadding(widget::Padding const &value) override;
 	};
 } // namespace widget
