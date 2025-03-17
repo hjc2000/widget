36b94bcbd99c1e721bbf8556bcea11f087c75e1d
hjc2000 <hjc.666@foxmail.com>
2025-03-17 17:12:10 +0800

    pch

---
+++ include/widget/pch.h
@@ -0,0 +1,27 @@
+#pragma once
+// IWYU pragma: begin_keep
+// IWYU pragma: begin_exports
+
+#include <base/pch.h>
+#include <QAbstractTableModel>
+#include <QApplication>
+#include <QCoreApplication>
+#include <QDateTimeEdit>
+#include <QDebug>
+#include <QDir>
+#include <QIODevice>
+#include <QMainWindow>
+#include <qnamespace.h>
+#include <QObject>
+#include <QPainter>
+#include <QPaintEvent>
+#include <QPalette>
+#include <QStyledItemDelegate>
+#include <QtCore>
+#include <qtextoption.h>
+#include <QtGlobal>
+#include <QVariant>
+#include <QWidget>
+
+// IWYU pragma: end_exports
+// IWYU pragma: end_keep
--- private_src/pch.h
+++ private_src/pch.h
@@ -2,26 +2,7 @@
 // IWYU pragma: begin_keep
 // IWYU pragma: begin_exports
 
-#include <base/pch.h>
-#include <QAbstractTableModel>
-#include <QApplication>
-#include <QCoreApplication>
-#include <QDateTimeEdit>
-#include <QDebug>
-#include <QDir>
-#include <QIODevice>
-#include <QMainWindow>
-#include <qnamespace.h>
-#include <QObject>
-#include <QPainter>
-#include <QPaintEvent>
-#include <QPalette>
-#include <QStyledItemDelegate>
-#include <QtCore>
-#include <qtextoption.h>
-#include <QtGlobal>
-#include <QVariant>
-#include <QWidget>
+#include <widget/pch.h>
 
 // IWYU pragma: end_exports
 // IWYU pragma: end_keep
