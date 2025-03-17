9036709cb9116cd1dd235d2e797b2521abbda3d2
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-03-18 00:24:14 +0800

    注释

--- include/widget/io/StreamIODevice.h
+++ include/widget/io/StreamIODevice.h
@@ -4,10 +4,10 @@
 
 namespace widget
 {
-	/**
-	 * @brief 将 base::Stream 包装成 QIODevice
-	 *
-	 */
+	///
+	/// @brief 将 base::Stream 包装成 QIODevice
+	///
+	///
 	class StreamIODevice :
 		public QIODevice
 	{
@@ -15,15 +15,14 @@ namespace widget
 		std::shared_ptr<base::Stream> _stream;
 
 		virtual qint64 readData(char *data, qint64 maxlen) override;
-
 		virtual qint64 writeData(char const *data, qint64 len) override;
 
 	public:
-		/**
-		 * @brief 构造函数。
-		 *
-		 * @param stream 传入一个流，将被包装成 QIODevice.
-		 */
+		///
+		/// @brief 构造函数。
+		///
+		/// @param stream 传入一个流，将被包装成 QIODevice.
+		///
 		StreamIODevice(std::shared_ptr<base::Stream> const &stream);
 	};
 } // namespace widget
