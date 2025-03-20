10966117dee79c8e2831227a5fce4455b9cfe0fc
hjc2000 <hjc.666@foxmail.com>
2025-03-20 09:18:11 +0800

    include

--- include/widget/pch.h
+++ include/widget/pch.h
@@ -2,37 +2,37 @@
 // IWYU pragma: begin_keep
 // IWYU pragma: begin_exports
 
-#include <base/pch.h>
-#include <QAbstractTableModel>
-#include <QApplication>
-#include <QBoxLayout>
-#include <QCheckBox>
-#include <QCoreApplication>
-#include <QDateTimeEdit>
-#include <QDebug>
-#include <QDir>
-#include <qfiledialog.h>
-#include <QHeaderView>
-#include <QIODevice>
-#include <QMainWindow>
-#include <qmessagebox.h>
-#include <qnamespace.h>
-#include <QObject>
-#include <qpainter.h>
-#include <QPainter>
-#include <QPaintEvent>
-#include <QPalette>
-#include <QPen>
-#include <QPushButton>
-#include <qstyleditemdelegate.h>
-#include <QStyledItemDelegate>
-#include <qstyleoption.h>
-#include <QTableView>
-#include <QtCore>
-#include <qtextoption.h>
-#include <QtGlobal>
-#include <QVariant>
-#include <QWidget>
+#include "base/pch.h"
+#include "QAbstractTableModel"
+#include "QApplication"
+#include "QBoxLayout"
+#include "QCheckBox"
+#include "QCoreApplication"
+#include "QDateTimeEdit"
+#include "QDebug"
+#include "QDir"
+#include "qfiledialog.h"
+#include "QHeaderView"
+#include "QIODevice"
+#include "QMainWindow"
+#include "qmessagebox.h"
+#include "qnamespace.h"
+#include "QObject"
+#include "QPainter"
+#include "qpainter.h"
+#include "QPaintEvent"
+#include "QPalette"
+#include "QPen"
+#include "QPushButton"
+#include "QStyledItemDelegate"
+#include "qstyleditemdelegate.h"
+#include "qstyleoption.h"
+#include "QTableView"
+#include "QtCore"
+#include "qtextoption.h"
+#include "QtGlobal"
+#include "QVariant"
+#include "QWidget"
 
 // IWYU pragma: end_exports
 // IWYU pragma: end_keep
--- private_src/pch.h
+++ private_src/pch.h
@@ -2,7 +2,7 @@
 // IWYU pragma: begin_keep
 // IWYU pragma: begin_exports
 
-#include <widget/pch.h>
+#include "widget/pch.h"
 
 // IWYU pragma: end_exports
 // IWYU pragma: end_keep
