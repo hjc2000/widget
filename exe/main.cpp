241e59ac034f50bc0101d25473db656c47d50072
hjc2000 <hjc.666@foxmail.com>
2025-03-28 17:07:35 +0800

    main

--- exe/main.cpp
+++ exe/main.cpp
@@ -1,24 +1,10 @@
 #include "widget/FusionApplication.h"
 #include "widget/MainWindow.h"
-#include <iostream>
 
 int main()
 {
-	try
-	{
-		widget::FusionApplication app{};
-		widget::MainWindow window{};
-		window.showMaximized();
-		return app.exec();
-	}
-	catch (std::exception const &e)
-	{
-		std::cerr << e.what() << std::endl;
-		return -1;
-	}
-	catch (...)
-	{
-		std::cerr << "未知异常" << std::endl;
-		return -1;
-	}
+	widget::FusionApplication app{};
+	widget::MainWindow window{};
+	window.showMaximized();
+	return app.exec();
 }
