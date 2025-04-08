fa9cd9190ea66cb80e996295b5910c68011100ae
hjc2000 <hjc.666@foxmail.com>
2025-04-08 16:04:07 +0800

    void widget::FormTableBox::SetItem(int row, widget::FormTableBoxItem const &item) 再设置新的表单项前会先移除该行的表单项（如果有的话）。

--- include/widget/box/FormTableBox.cpp
+++ include/widget/box/FormTableBox.cpp
@@ -26,12 +26,17 @@ void widget::FormTableBox::SetItem(int row, widget::FormTableBoxItem const &item
 
 	try
 	{
+		RemoveItem(row);
 		_layout.SetItem(row, item.LeftWidget().get(), item.RightWidget().get());
 		_widget_dic.Add(row, item);
 	}
 	catch (std::exception const &e)
 	{
-		throw new std::runtime_error{CODE_POS_STR + e.what()};
+		throw std::runtime_error{CODE_POS_STR + e.what()};
+	}
+	catch (...)
+	{
+		throw std::runtime_error{CODE_POS_STR + "未知的异常。"};
 	}
 }
 
@@ -58,8 +63,11 @@ void widget::FormTableBox::SetItem(std::initializer_list<widget::FormTableBoxIte
 
 void widget::FormTableBox::RemoveItem(int row)
 {
-	_layout.RemoveItem(row);
-	_widget_dic.Remove(row);
+	bool remove_result = _widget_dic.Remove(row);
+	if (remove_result)
+	{
+		_layout.RemoveItem(row);
+	}
 }
 
 widget::Padding widget::FormTableBox::Padding() const
