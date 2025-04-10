93a4ebad5c82d6fd1ec534cfda41a8fc12ede688
hjc2000 <hjc.666@foxmail.com>
2025-04-10 11:59:35 +0800

    LabelValueUnitGridItem

--- include/widget/box/LabelValueUnitGridItem.cpp
+++ include/widget/box/LabelValueUnitGridItem.cpp
@@ -1,1 +1,27 @@
 #include "LabelValueUnitGridItem.h"
+#include "base/string/define.h"
+#include <stdexcept>
+
+widget::LabelValueUnitGridItem::LabelValueUnitGridItem(std::shared_ptr<QWidget> label,
+													   std::shared_ptr<QWidget> data,
+													   std::shared_ptr<QWidget> unit)
+{
+	if (label == nullptr)
+	{
+		throw std::invalid_argument{CODE_POS_STR + "禁止传入空指针。"};
+	}
+
+	if (data == nullptr)
+	{
+		throw std::invalid_argument{CODE_POS_STR + "禁止传入空指针。"};
+	}
+
+	if (unit == nullptr)
+	{
+		throw std::invalid_argument{CODE_POS_STR + "禁止传入空指针。"};
+	}
+
+	_label = label;
+	_data = data;
+	_unit = unit;
+}
--- include/widget/box/LabelValueUnitGridItem.h
+++ include/widget/box/LabelValueUnitGridItem.h
@@ -14,8 +14,21 @@ namespace widget
 	public:
 		LabelValueUnitGridItem(std::shared_ptr<QWidget> label,
 							   std::shared_ptr<QWidget> data,
-							   std::shared_ptr<QWidget> unit)
+							   std::shared_ptr<QWidget> unit);
+
+		std::shared_ptr<QWidget> Lable() const
 		{
+			return _label;
+		}
+
+		std::shared_ptr<QWidget> Data() const
+		{
+			return _data;
+		}
+
+		std::shared_ptr<QWidget> Unit() const
+		{
+			return _unit;
 		}
 	};
 } // namespace widget
