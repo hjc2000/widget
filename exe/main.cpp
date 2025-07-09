1e8e71aabcf74d4eb5a84e05e6fa35ca1677d9a9
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-09 23:57:02 +0800

    "重构测试应用入口，分离Fusion和Core应用测试逻辑"

--- exe/main.cpp
+++ exe/main.cpp
@@ -3,17 +3,22 @@
 #include "widget/MainWindow.h"
 #include <iostream>
 
+int TestFusionApplication()
+{
+	widget::FusionApplication app{};
+	widget::MainWindow window{};
+	window.showMaximized();
+	return app.exec();
+}
+
+int TestCoreApplication()
+{
+	widget::CoreApplication app{};
+	std::cout << "这是一个Qt终端应用程序" << std::endl;
+	return app.exec();
+}
+
 int main()
 {
-	{
-		// widget::FusionApplication app{};
-		// widget::MainWindow window{};
-		// window.showMaximized();
-		// return app.exec();
-	}
-	{
-		widget::CoreApplication app{};
-		std::cout << "这是一个Qt终端应用程序" << std::endl;
-		return app.exec();
-	}
+	return TestCoreApplication();
 }
