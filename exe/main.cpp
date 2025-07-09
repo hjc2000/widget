d6a72d7837617f7400e1d7cc0531f486702f4610
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-10 00:08:31 +0800

    "添加串口配置功能到CoreApplication测试程序"

--- exe/main.cpp
+++ exe/main.cpp
@@ -1,3 +1,4 @@
+#include "qserialport.h"
 #include "widget/CoreApplication.h"
 #include "widget/FusionApplication.h"
 #include "widget/MainWindow.h"
@@ -14,6 +15,22 @@ int TestFusionApplication()
 int TestCoreApplication()
 {
 	widget::CoreApplication app{};
+	QSerialPort serial{};
+	serial.setPortName("COM4");
+	serial.setBaudRate(115200);
+
+	// 设置数据位
+	serial.setDataBits(QSerialPort::DataBits::Data8);
+
+	// 设置校验位
+	serial.setParity(QSerialPort::Parity::NoParity);
+
+	// 设置停止位
+	serial.setStopBits(QSerialPort::StopBits::OneStop);
+
+	// 设置流控制
+	serial.setFlowControl(QSerialPort::FlowControl::NoFlowControl);
+
 	std::cout << "这是一个Qt终端应用程序" << std::endl;
 	return app.exec();
 }
