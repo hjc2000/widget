ad8e01a3b1db5b6c76412eb550d1f5b700e6bb88
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-09 23:24:51 +0800

    "添加Qt串口模块依赖并简化测试程序链接配置"

--- project.cmake
+++ project.cmake
@@ -5,6 +5,7 @@ target_import_qt_widgets(${ProjectName} PUBLIC)
 target_import_qt_opengl(${ProjectName} PUBLIC)
 target_import_qwt(${ProjectName} PUBLIC)
 target_import_qxlsx(${ProjectName} PUBLIC)
+target_import_qt_serial_port(${ProjectName} PUBLIC)
 
 # 添加测试程序
 if(1)
@@ -17,10 +18,6 @@ if(1)
 	endif()
 
 	add_executable(${exe_name} ${exe_type} ${CMAKE_CURRENT_SOURCE_DIR}/exe/main.cpp)
-
-	target_link_libraries(${ProjectName} PUBLIC -Wl,--start-group)
 	target_link_libraries(${exe_name} PUBLIC ${ProjectName})
-	target_link_libraries(${ProjectName} PUBLIC -Wl,--end-group)
-
 	target_install(${exe_name})
 endif()
