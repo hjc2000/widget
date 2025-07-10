adc9a6147eafac98d96aa37c27eefe53ccdc66a0
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-11 01:18:21 +0800

    "为Serial类添加线程安全的串口资源管理，通过weak_ptr避免循环引用"

--- include/widget/io/Serial.h
+++ include/widget/io/Serial.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "base/embedded/serial/serial_parameter.h"
+#include "qserialport.h"
 #include "serial_handle.h"
+#include "widget/thread/Thread.h"
+#include <memory>
 #include <string>
 
 namespace widget
@@ -9,9 +12,28 @@ namespace widget
 		public base::serial::serial_handle
 	{
 	private:
+		widget::Thread _thread{};
+		std::weak_ptr<QSerialPort> _serial{};
+
 	public:
 		Serial(std::string const &name)
 		{
+			_thread.start();
+
+			std::shared_ptr<base::task::ITask> task = _thread.InvokeAsync(
+				[this]()
+				{
+					{
+						// 后续只能通过 std::weak_ptr<QSerialPort> _serial{} 访问，
+						// 特别是 lambda 表达式不能直接按值捕获 std::shared_ptr<QSerialPort> serial,
+						// 否则会出现循环引用。
+						std::shared_ptr<QSerialPort> serial{new QSerialPort{}};
+						_thread.AddResource(serial);
+						_serial = serial;
+					}
+				});
+
+			task->Wait();
 		}
 
 		~Serial()
