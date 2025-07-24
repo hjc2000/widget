c3f86960c693b54950eb3cc4b16a46c4f5398a71
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-24 23:57:43 +0800

    重构FormTableBox实现，将方法定义从cpp移至头文件并添加IWYU注释

--- include/widget/box/FormTableBox.h
+++ include/widget/box/FormTableBox.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "GridBox.h"
+#include "GridBoxItem.h"
 #include "QWidget"
 #include "widget/box/FormTableBoxItem.h"
 #include "widget/layout/Padding.h"
@@ -21,14 +22,40 @@ namespace widget
 	public:
 		/* #region 构造函数 */
 
-		FormTableBox();
+		FormTableBox()
+		{
+			_layout.AddWidget(&_box);
+
+			// 第 0 列的增长因子为 0，不允许增长，而是根据内容调整大小。
+			_box.SetColumnStretch(0, 0);
+
+			// 第 1 列的增长因子为 1.
+			//
+			// 凡是增长因子不为 0 的，都会根据因子的比例去分配剩余的宽度。
+			// 这里一共 2 列，其中第 0 列增长因子为 0，所以第 1 列就占据所有剩余宽度。
+			_box.SetColumnStretch(1, 1);
+
+			// 让表单中每一行从顶端开始一行一行往下排列。
+			_box.SetAlignment(Qt::AlignmentFlag::AlignTop);
+		}
 
 		///
 		/// @brief 构造函数。构造的过程中添加一系列表单项到盒子中。
 		///
 		/// @param items
 		///
-		FormTableBox(std::initializer_list<widget::FormTableBoxItem> const &items);
+		FormTableBox(std::initializer_list<widget::FormTableBoxItem> const &items)
+			: FormTableBox()
+		{
+			try
+			{
+				SetItem(items);
+			}
+			catch (std::exception const &e)
+			{
+				throw new std::runtime_error{CODE_POS_STR + e.what()};
+			}
+		}
 
 		/* #endregion */
 
@@ -40,7 +67,39 @@ namespace widget
 		///
 		/// @param item
 		///
-		void SetItem(int row, widget::FormTableBoxItem const &item);
+		void SetItem(int row, widget::FormTableBoxItem const &item)
+		{
+			if (item.LeftWidget() == nullptr)
+			{
+				throw new std::runtime_error{CODE_POS_STR + "item.LeftWidget() 不能为空指针。"};
+			}
+
+			if (item.RightWidget() == nullptr)
+			{
+				throw new std::runtime_error{CODE_POS_STR + "item.RightWidget() 不能为空指针。"};
+			}
+
+			try
+			{
+				// 水平展开，充满表单网格，
+				//
+				// 垂直固定高度，防止垂直方向将表单盒子撑开，使得表单盒子充满父容器，
+				// 然后每一行非常高。
+				item.LeftWidget()->setSizePolicy(QSizePolicy::Policy::Expanding, QSizePolicy::Policy::Fixed);
+				item.RightWidget()->setSizePolicy(QSizePolicy::Policy::Expanding, QSizePolicy::Policy::Fixed);
+
+				_box.SetItem(widget::GridBoxItem{row, 0, item.LeftWidget()});
+				_box.SetItem(widget::GridBoxItem{row, 1, item.RightWidget()});
+			}
+			catch (std::exception const &e)
+			{
+				throw std::runtime_error{CODE_POS_STR + e.what()};
+			}
+			catch (...)
+			{
+				throw std::runtime_error{CODE_POS_STR + "未知的异常。"};
+			}
+		}
 
 		///
 		/// @brief 设置一系列表单项。
@@ -50,20 +109,46 @@ namespace widget
 		///
 		/// @param items
 		///
-		void SetItem(std::initializer_list<widget::FormTableBoxItem> const &items);
+		void SetItem(std::initializer_list<widget::FormTableBoxItem> const &items)
+		{
+			int i = 0;
+
+			try
+			{
+				for (widget::FormTableBoxItem const &item : items)
+				{
+					SetItem(i++, item);
+				}
+			}
+			catch (std::exception const &e)
+			{
+				std::string message = CODE_POS_STR +
+									  std::format("添加到第 {} 个项目时引发异常", i) +
+									  e.what();
+
+				throw new std::runtime_error{message};
+			}
+		}
 
 		///
 		/// @brief 移除某一行的表单项。
 		///
 		/// @param row
 		///
-		void RemoveItem(int row);
+		void RemoveItem(int row)
+		{
+			_box.RemoveWidget(row, 0);
+			_box.RemoveWidget(row, 1);
+		}
 
 		///
 		/// @brief 清空所有表单项。
 		///
 		///
-		void ClearItems();
+		void ClearItems()
+		{
+			_box.ClearWidgets();
+		}
 
 		///
 		/// @brief 表单盒子内边距。
