3e00a8efd326f2288165d083a7ae12a5c6864213
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-24 23:47:42 +0800

    重构HBoxLayout实现，将方法定义从cpp移至头文件并添加IWYU注释

--- include/widget/layout/HBoxLayout.h
+++ include/widget/layout/HBoxLayout.h
@@ -21,7 +21,17 @@ namespace widget
 		///
 		/// @param parent 要被布局控件进行 UI 布局的 QWidget 对象。
 		///
-		HBoxLayout(QWidget *parent);
+		HBoxLayout(QWidget *parent)
+		{
+			_layout = std::shared_ptr<QHBoxLayout>{new QHBoxLayout{parent}};
+
+			_layout->setAlignment(Qt::Alignment{Qt::AlignmentFlag::AlignTop | Qt::AlignmentFlag::AlignLeft});
+
+			// 控件之间的间距。
+			_layout->setSpacing(10);
+
+			SetPadding(widget::Padding{});
+		}
 
 		///
 		/// @brief 构造一个水平布局的盒子。
@@ -29,34 +39,60 @@ namespace widget
 		/// @param parent 要被布局控件进行 UI 布局的 QWidget 对象。
 		/// @param paddint 盒子内边距。
 		///
-		HBoxLayout(QWidget *parent, widget::Padding const &paddint);
+		HBoxLayout(QWidget *parent, widget::Padding const &paddint)
+			: widget::HBoxLayout{parent}
+		{
+			SetPadding(paddint);
+		}
 
 		///
 		/// @brief 向布局添加一个控件。
 		///
 		/// @param widget 要被添加到布局中进行显示的 QWidget 对象。
 		///
-		void AddWidget(QWidget *widget);
+		void AddWidget(QWidget *widget)
+		{
+			_layout->addWidget(widget);
+		}
 
 		///
 		/// @brief 将控件从布局中移除。
 		///
 		/// @param widget
 		///
-		void RemoveWidget(QWidget *widget);
+		void RemoveWidget(QWidget *widget)
+		{
+			_layout->removeWidget(widget);
+		}
 
 		///
 		/// @brief 获取内边距。
 		///
 		/// @return widget::Padding
 		///
-		widget::Padding Padding() const;
+		widget::Padding Padding() const
+		{
+			QMargins value = _layout->contentsMargins();
+
+			return widget::Padding{
+				value.left(),
+				value.top(),
+				value.right(),
+				value.bottom(),
+			};
+		}
 
 		///
 		/// @brief 设置内边距。
 		///
 		/// @param value
 		///
-		void SetPadding(widget::Padding const &value);
+		void SetPadding(widget::Padding const &value)
+		{
+			// 布局内的内容区域与布局边框的间距。实际上相当于网页中的盒子内边距。
+			_layout->setContentsMargins(value.Left(), value.Top(),
+										value.Right(), value.Bottom());
+		}
 	};
+
 } // namespace widget
