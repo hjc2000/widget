30e4bfb234fb49054a2b04c03e1d41044bdcd0c4
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-27 22:55:24 +0800

    重构HBox实现，将方法定义从cpp移至头文件并添加IWYU注释

--- include/widget/box/HBox.h
+++ include/widget/box/HBox.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "base/container/iterator/IEnumerable.h"
+#include "base/string/define.h"
 #include "qwidget.h"
 #include "widget/layout/HBoxLayout.h"
 #include "widget/layout/Padding.h"
@@ -19,9 +20,21 @@ namespace widget
 		/* #region 构造函数 */
 
 		HBox() = default;
-		HBox(std::initializer_list<std::shared_ptr<QWidget>> const &widgets);
-		HBox(std::vector<std::shared_ptr<QWidget>> const &widgets);
-		HBox(base::IEnumerable<std::shared_ptr<QWidget>> const &widgets);
+
+		HBox(std::initializer_list<std::shared_ptr<QWidget>> const &widgets)
+		{
+			AddWidget(widgets);
+		}
+
+		HBox(std::vector<std::shared_ptr<QWidget>> const &widgets)
+		{
+			AddWidget(widgets);
+		}
+
+		HBox(base::IEnumerable<std::shared_ptr<QWidget>> const &widgets)
+		{
+			AddWidget(widgets);
+		}
 
 		template <size_t TCount>
 		HBox(std::array<std::shared_ptr<QWidget>, TCount> const &widgets)
@@ -38,28 +51,62 @@ namespace widget
 		///
 		/// @param widget
 		///
-		void AddWidget(std::shared_ptr<QWidget> const &widget);
+		void AddWidget(std::shared_ptr<QWidget> const &widget)
+		{
+			if (widget == nullptr)
+			{
+				throw std::invalid_argument{CODE_POS_STR + "widget 不能是空指针"};
+			}
+
+			auto it = _widget_set.find(widget);
+			if (it != _widget_set.end())
+			{
+				// 如果已经添加过了，就不重复添加了。
+				return;
+			}
+
+			_widget_set.insert(widget);
+			_layout.AddWidget(widget.get());
+		}
 
 		///
 		/// @brief 向盒子添加一系列控件。
 		///
 		/// @param widgets
 		///
-		void AddWidget(std::initializer_list<std::shared_ptr<QWidget>> const &widgets);
+		void AddWidget(std::initializer_list<std::shared_ptr<QWidget>> const &widgets)
+		{
+			for (std::shared_ptr<QWidget> const &widget : widgets)
+			{
+				AddWidget(widget);
+			}
+		}
 
 		///
 		/// @brief 向盒子添加一系列控件。
 		///
 		/// @param widgets
 		///
-		void AddWidget(std::vector<std::shared_ptr<QWidget>> const &widgets);
+		void AddWidget(std::vector<std::shared_ptr<QWidget>> const &widgets)
+		{
+			for (auto widget : widgets)
+			{
+				AddWidget(widget);
+			}
+		}
 
 		///
 		/// @brief 向盒子添加一系列控件。
 		///
 		/// @param widgets
 		///
-		void AddWidget(base::IEnumerable<std::shared_ptr<QWidget>> const &widgets);
+		void AddWidget(base::IEnumerable<std::shared_ptr<QWidget>> const &widgets)
+		{
+			for (auto widget : widgets)
+			{
+				AddWidget(widget);
+			}
+		}
 
 		///
 		/// @brief 向盒子添加一系列控件。
@@ -83,26 +130,51 @@ namespace widget
 		///
 		/// @param widget
 		///
-		void RemoveWidget(std::shared_ptr<QWidget> const &widget);
+		void RemoveWidget(std::shared_ptr<QWidget> const &widget)
+		{
+			auto it = _widget_set.find(widget);
+			if (it == _widget_set.end())
+			{
+				return;
+			}
+
+			_widget_set.erase(widget);
+			_layout.RemoveWidget(widget.get());
+		}
 
 		///
 		/// @brief 清空盒子中的所有控件。
 		///
 		///
-		void ClearWidget();
+		void ClearWidget()
+		{
+			for (auto widget : _widget_set)
+			{
+				_layout.RemoveWidget(widget.get());
+			}
+
+			_widget_set.clear();
+		}
 
 		///
 		/// @brief 获取内边距。
 		///
-		/// @return widget::Padding
+		/// @return
 		///
-		widget::Padding Padding() const;
+		widget::Padding Padding() const
+		{
+			return _layout.Padding();
+		}
 
 		///
 		/// @brief 设置内边距。
 		///
 		/// @param value
 		///
-		void SetPadding(widget::Padding const &value);
+		void SetPadding(widget::Padding const &value)
+		{
+			_layout.SetPadding(value);
+		}
 	};
+
 } // namespace widget
