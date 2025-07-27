c8911d5f6ad31e4cc9d8296dd9591447e83a2a61
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-27 23:00:21 +0800

    重构GridBoxItem实现，将方法定义从cpp移至头文件并添加IWYU注释

--- include/widget/box/GridBoxItem.h
+++ include/widget/box/GridBoxItem.h
@@ -1,10 +1,12 @@
 #pragma once
+#include "base/string/define.h"
 #include "qwidget.h"
 #include <memory>
+#include <stdexcept>
 
 namespace widget
 {
-	class GridBoxItem
+	class GridBoxItem final
 	{
 	private:
 		int _row = 0;
@@ -21,18 +23,64 @@ namespace widget
 		std::shared_ptr<QWidget> _widget;
 
 	public:
-		GridBoxItem(int row, int column, std::shared_ptr<QWidget> widget);
+		GridBoxItem(int row, int column, std::shared_ptr<QWidget> widget)
+		{
+			if (widget == nullptr)
+			{
+				throw std::invalid_argument{CODE_POS_STR + "禁止传入空指针。"};
+			}
 
-		GridBoxItem(int row, int column, Qt::AlignmentFlag align, std::shared_ptr<QWidget> widget);
+			_row = row;
+			_column = column;
+			_widget = widget;
+		}
+
+		GridBoxItem(int row, int column, Qt::AlignmentFlag align, std::shared_ptr<QWidget> widget)
+		{
+			if (widget == nullptr)
+			{
+				throw std::invalid_argument{CODE_POS_STR + "禁止传入空指针。"};
+			}
+
+			_row = row;
+			_column = column;
+			_align = align;
+			_widget = widget;
+		}
 
 		GridBoxItem(int row, int column,
 					int row_span, int column_span,
-					std::shared_ptr<QWidget> widget);
+					std::shared_ptr<QWidget> widget)
+		{
+			if (widget == nullptr)
+			{
+				throw std::invalid_argument{CODE_POS_STR + "禁止传入空指针。"};
+			}
+
+			_row = row;
+			_column = column;
+			_row_span = row_span;
+			_column_span = column_span;
+			_widget = widget;
+		}
 
 		GridBoxItem(int row, int column,
 					int row_span, int column_span,
 					Qt::AlignmentFlag align,
-					std::shared_ptr<QWidget> widget);
+					std::shared_ptr<QWidget> widget)
+		{
+			if (widget == nullptr)
+			{
+				throw std::invalid_argument{CODE_POS_STR + "禁止传入空指针。"};
+			}
+
+			_row = row;
+			_column = column;
+			_row_span = row_span;
+			_column_span = column_span;
+			_align = align;
+			_widget = widget;
+		}
 
 		int Row() const
 		{
