12a6eb208ab8067ec3cd92d7323c5e499ad82709
hjc2000 <hjc.666@foxmail.com>
2025-07-28 13:49:27 +0800

    重构convert实现，将所有方法从cpp文件移动到头文件并内联实现

--- include/widget/convert.h
+++ include/widget/convert.h
@@ -10,9 +10,16 @@ namespace base
 	/// @brief 将 QString 转换为 std::string.
 	///
 	/// @param qstr
-	/// @return std::string
+	/// @return
 	///
-	std::string to_string(QString const &qstr);
+	inline std::string to_string(QString const &qstr)
+	{
+		// 使用 toUtf8() 将 QString 转换为 QByteArray
+		QByteArray utf8Bytes = qstr.toUtf8();
+
+		// 将 QByteArray 转换为 std::string
+		return std::string{utf8Bytes.constData(), static_cast<size_t>(utf8Bytes.size())};
+	}
 
 } // namespace base
 
@@ -22,26 +29,70 @@ namespace widget
 	/// @brief 将 std::string 转换为 QString.
 	///
 	/// @param str
-	/// @return QString
+	/// @return
 	///
-	QString ToQString(std::string const &str);
+	inline QString ToQString(std::string const &str)
+	{
+		return QString{str.c_str()};
+	}
 
-	QString ToQString(char const *str);
+	inline QString ToQString(char const *str)
+	{
+		return QString{str};
+	}
 
 	///
 	/// @brief 将 Qt::CheckState 转换为 widget::CheckState.
 	///
 	/// @param q_check_state
-	/// @return widget::CheckState
 	///
-	widget::CheckState ToCheckState(Qt::CheckState const q_check_state);
+	/// @return
+	///
+	inline widget::CheckState ToCheckState(Qt::CheckState const q_check_state)
+	{
+		switch (q_check_state)
+		{
+		case Qt::CheckState::Checked:
+			{
+				return widget::CheckState::Checked;
+			}
+		case Qt::CheckState::PartiallyChecked:
+			{
+				return widget::CheckState::PartiallyChecked;
+			}
+		default:
+		case Qt::CheckState::Unchecked:
+			{
+				return widget::CheckState::Unchecked;
+			}
+		}
+	}
 
 	///
 	/// @brief 将 widget::CheckState 转换为 Qt::CheckState.
 	///
 	/// @param check_state
-	/// @return Qt::CheckState
 	///
-	Qt::CheckState ToQtCheckState(widget::CheckState const &check_state);
+	/// @return
+	///
+	inline Qt::CheckState ToQtCheckState(widget::CheckState const &check_state)
+	{
+		switch (check_state)
+		{
+		case widget::CheckState::Checked:
+			{
+				return Qt::CheckState::Checked;
+			}
+		case widget::CheckState::PartiallyChecked:
+			{
+				return Qt::CheckState::PartiallyChecked;
+			}
+		default:
+		case widget::CheckState::Unchecked:
+			{
+				return Qt::CheckState::Unchecked;
+			}
+		}
+	}
 
 } // namespace widget
