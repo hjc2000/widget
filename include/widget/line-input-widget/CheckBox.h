9b659c20bd8c36aaf5ef9695ae2ecd4cc51ddbca
hjc2000 <hjc.666@foxmail.com>
2025-07-28 11:43:47 +0800

    重构CheckBox实现，将实现代码从cpp文件移动到头文件

--- include/widget/line-input-widget/CheckBox.h
+++ include/widget/line-input-widget/CheckBox.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "base/delegate/Delegate.h"
 #include "base/delegate/IEvent.h"
+#include "base/string/define.h"
 #include "QCheckBox"
 #include "QWidget"
+#include "widget/convert.h"
 #include "widget/layout/HBoxLayout.h"
 #include "widget/line-input-widget/CheckState.h"
+#include <iostream>
 
 namespace widget
 {
@@ -21,31 +24,77 @@ namespace widget
 
 		base::Delegate<> _check_state_changed_event;
 
-		void ConnectSignals();
+		void ConnectSignals()
+		{
+			connect(_check_box.get(),
+					&QCheckBox::checkStateChanged,
+					[this](Qt::CheckState q_check_state)
+					{
+						try
+						{
+							_check_state_changed_event.Invoke();
+						}
+						catch (std::exception const &e)
+						{
+							std::cerr << CODE_POS_STR + e.what() << std::endl;
+						}
+						catch (...)
+						{
+							std::cerr << CODE_POS_STR + "发生了未知异常。" << std::endl;
+						}
+					});
+		}
 
 	public:
-		CheckBox();
-		CheckBox(std::string const &text);
+		CheckBox()
+		{
+			_check_box = std::shared_ptr<QCheckBox>{new QCheckBox{}};
+			_layout.AddWidget(_check_box.get());
+
+			ConnectSignals();
+		}
+
+		CheckBox(std::string const &text)
+		{
+			_check_box = std::shared_ptr<QCheckBox>{new QCheckBox{
+				text.c_str(),
+				nullptr,
+			}};
+
+			_layout.AddWidget(_check_box.get());
+
+			ConnectSignals();
+		}
 
 		///
 		/// @brief 选择状态。
 		///
-		/// @return widget::CheckState
+		/// @return
 		///
-		widget::CheckState CheckState() const;
+		widget::CheckState CheckState() const
+		{
+			return widget::ToCheckState(_check_box->checkState());
+		}
 
 		///
 		/// @brief 设置选择状态。
 		///
 		/// @param value
 		///
-		void SetCheckState(widget::CheckState value);
+		void SetCheckState(widget::CheckState value)
+		{
+			_check_box->setCheckState(widget::ToQtCheckState(value));
+		}
 
 		///
 		/// @brief 选择状态改变事件。
 		///
-		/// @return base::IEvent<>&
+		/// @return
 		///
-		base::IEvent<> &CheckStateChangedEvent();
+		base::IEvent<> &CheckStateChangedEvent()
+		{
+			return _check_state_changed_event;
+		}
 	};
+
 } // namespace widget
