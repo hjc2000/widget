31ce3fb7c1a14ef559d412be5c86e90d6987b8be
hjc2000 <hjc.666@foxmail.com>
2025-07-28 11:33:17 +0800

    重构布局组件，添加FixSizeSpacingBlock固定大小空白块控件

---
+++ include/widget/box/FixSizeSpacingBlock.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "qwidget.h"
+
+namespace widget
+{
+	///
+	/// @brief 固定大小的空白块。
+	///
+	///
+	class FixSizeSpacingBlock :
+		public QWidget
+	{
+	public:
+		FixSizeSpacingBlock(int width, int height)
+			: QWidget(nullptr)
+		{
+			setFixedSize(width, height);
+		}
+	};
+
+} // namespace widget
