59ba4835ac1bf12ed16b0c55144a1bbbf0df48e1
hjc2000 <hjc.666@foxmail.com>
2025-07-28 14:00:19 +0800

    重构StreamIODevice和IndicatorLight实现，将所有方法从cpp文件移动到头文件并内联实现，标记类为final

--- include/widget/indicator/IndicatorLight.h
+++ include/widget/indicator/IndicatorLight.h
@@ -12,7 +12,7 @@ namespace widget
 	/// @brief 指示灯控件。
 	///
 	///
-	class IndicatorLight :
+	class IndicatorLight final :
 		public QWidget
 	{
 	private:
@@ -21,7 +21,32 @@ namespace widget
 		QColor _off_color = Qt::GlobalColor::gray;
 
 	protected:
-		virtual void paintEvent(QPaintEvent *event) override;
+		virtual void paintEvent(QPaintEvent *event) override
+		{
+			QPainter painter{this};
+
+			// 开启抗锯齿
+			painter.setRenderHint(QPainter::Antialiasing);
+
+			QColor color = CurrentColor();
+			painter.setBrush(color);
+
+			int border_width = 1;
+			QPen pen{Qt::GlobalColor::black};
+			pen.setWidth(border_width);
+			painter.setPen(pen);
+
+			// 调整绘图区域，避免边线超出控件范围。
+			// 根据边线宽度调整
+			//
+			// 如果边线宽度不为 0，绘制边线一定会超出控件范围，导致虽然绘制的是圆形，看着是矩形。
+			// 因为控件的视图窗口是矩形，超出范围的边线就看不见了，导致边线外围好像被用一个矩形
+			// 模具切割。
+			QRect adjustedRect = rect().adjusted(border_width, border_width, -border_width, -border_width);
+
+			// 绘制圆形
+			painter.drawEllipse(adjustedRect);
+		}
 
 	public:
 		/* #region 构造函数 */
@@ -92,13 +117,31 @@ namespace widget
 		/// @brief 打开指示灯。
 		///
 		///
-		void TurnOn();
+		void TurnOn()
+		{
+			if (_state == base::led::State::On)
+			{
+				return;
+			}
+
+			_state = base::led::State::On;
+			update();
+		}
 
 		///
 		/// @brief 关闭指示灯。
 		///
 		///
-		void TurnOff();
+		void TurnOff()
+		{
+			if (_state == base::led::State::Off)
+			{
+				return;
+			}
+
+			_state = base::led::State::Off;
+			update();
+		}
 
 		///
 		/// @brief 翻转指示灯。
--- include/widget/io/StreamIODevice.h
+++ include/widget/io/StreamIODevice.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "base/stream/Stream.h"
+#include "base/string/define.h"
 #include "QIODevice"
+#include <iostream>
 
 namespace widget
 {
@@ -8,14 +10,59 @@ namespace widget
 	/// @brief 将 base::Stream 包装成 QIODevice
 	///
 	///
-	class StreamIODevice :
+	class StreamIODevice final :
 		public QIODevice
 	{
 	private:
 		std::shared_ptr<base::Stream> _stream;
 
-		virtual qint64 readData(char *data, qint64 maxlen) override;
-		virtual qint64 writeData(char const *data, qint64 len) override;
+		virtual qint64 readData(char *data, qint64 maxlen) override
+		{
+			try
+			{
+				int32_t size = std::min<int64_t>(INT32_MAX, maxlen);
+
+				base::Span span{
+					reinterpret_cast<uint8_t *>(data),
+					size,
+				};
+
+				return _stream->Read(span);
+			}
+			catch (std::exception const &e)
+			{
+				std::cerr << CODE_POS_STR + e.what() << std::endl;
+				return -1;
+			}
+		}
+
+		virtual qint64 writeData(char const *data, qint64 len) override
+		{
+			try
+			{
+				int64_t have_written = 0;
+
+				while (have_written < len)
+				{
+					int32_t size = std::min<int64_t>(INT32_MAX, len - have_written);
+
+					base::ReadOnlySpan span{
+						reinterpret_cast<uint8_t const *>(data + have_written),
+						size,
+					};
+
+					_stream->Write(span);
+					have_written += size;
+				}
+
+				return have_written;
+			}
+			catch (std::exception const &e)
+			{
+				std::cerr << CODE_POS_STR + e.what() << std::endl;
+				return -1;
+			}
+		}
 
 	public:
 		///
@@ -23,6 +70,14 @@ namespace widget
 		///
 		/// @param stream 传入一个流，将被包装成 QIODevice.
 		///
-		StreamIODevice(std::shared_ptr<base::Stream> const &stream);
+		StreamIODevice(std::shared_ptr<base::Stream> const &stream)
+			: _stream(stream)
+		{
+			if (_stream == nullptr)
+			{
+				throw std::invalid_argument{CODE_POS_STR + "stream 不能是空指针。"};
+			}
+		}
 	};
+
 } // namespace widget
