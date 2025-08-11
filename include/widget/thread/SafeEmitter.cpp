e74a840667f078fa8afc47d183ff5ba1f1112255
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-12 00:23:47 +0800

    调整SafeEmitter::Emit方法的代码格式，增强可读性

--- include/widget/thread/SafeEmitter.cpp
+++ include/widget/thread/SafeEmitter.cpp
@@ -10,25 +10,26 @@ void widget::SafeEmitter::Emit()
 		throw base::ObjectDisposedException{CODE_POS_STR + "对象已经释放，禁止发射信号。"};
 	}
 
-	QMetaObject::invokeMethod(this, [this]()
-							  {
-								  if (_disposed)
-								  {
-									  return;
-								  }
+	QMetaObject::invokeMethod(
+		this, [this]()
+		{
+			if (_disposed)
+			{
+				return;
+			}
 
-								  try
-								  {
-									  _callback.Invoke();
-								  }
-								  catch (std::exception const &e)
-								  {
-									  std::cerr << CODE_POS_STR << e.what() << std::endl;
-								  }
-								  catch (...)
-								  {
-									  std::cerr << CODE_POS_STR << "未知的异常。" << std::endl;
-								  }
-							  },
-							  Qt::QueuedConnection);
+			try
+			{
+				_callback.Invoke();
+			}
+			catch (std::exception const &e)
+			{
+				std::cerr << CODE_POS_STR << e.what() << std::endl;
+			}
+			catch (...)
+			{
+				std::cerr << CODE_POS_STR << "未知的异常。" << std::endl;
+			}
+		},
+		Qt::QueuedConnection);
 }
