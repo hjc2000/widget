631cb10a88654a764ce0e97d94da4778988964bc
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-11 22:05:37 +0800

    将Input类的信号连接实现从头文件移动到源文件

--- include/widget/line-input-widget/Input.cpp
+++ include/widget/line-input-widget/Input.cpp
@@ -1,1 +1,71 @@
 #include "Input.h" // IWYU pragma: keep
+
+void widget::Input::ConnectSignal()
+{
+	QMetaObject::Connection connection;
+
+	connection = connect(&_line_edit,
+						 &QLineEdit::textChanged,
+						 [this]()
+						 {
+							 try
+							 {
+								 _text_changed_event.Invoke(_line_edit.text());
+							 }
+							 catch (std::exception const &e)
+							 {
+							 }
+							 catch (...)
+							 {
+							 }
+						 });
+
+	_connections.push_back(connection);
+
+	connection = connect(&_line_edit,
+						 &QLineEdit::textEdited,
+						 [this]()
+						 {
+							 try
+							 {
+								 _text_edited_event.Invoke(_line_edit.text());
+							 }
+							 catch (std::exception const &e)
+							 {
+							 }
+							 catch (...)
+							 {
+							 }
+						 });
+
+	_connections.push_back(connection);
+
+	connection = connect(&_line_edit,
+						 &QLineEdit::editingFinished,
+						 [this]()
+						 {
+							 try
+							 {
+								 _text_editing_finished_event.Invoke(_line_edit.text());
+							 }
+							 catch (std::exception const &e)
+							 {
+							 }
+							 catch (...)
+							 {
+							 }
+
+							 try
+							 {
+								 _text_changing_finished_event.Invoke(_line_edit.text());
+							 }
+							 catch (std::exception const &e)
+							 {
+							 }
+							 catch (...)
+							 {
+							 }
+						 });
+
+	_connections.push_back(connection);
+}
