b28d3e13a0e40c14d95484ed6796d0cf2d85b457
hjc2000 <hjc.666@foxmail.com>
2025-08-11 09:47:32 +0800

    实现MemoryMapFile的OpenOrCreate接口，返回共享指针

--- include/widget/io/MemoryMapFile.cpp
+++ include/widget/io/MemoryMapFile.cpp
@@ -1,1 +1,7 @@
 #include "MemoryMapFile.h" // IWYU pragma: keep
+#include <memory>
+
+std::shared_ptr<base::memory_map_file::IMemoryMapFile> base::memory_map_file::OpenOrCreate(base::Path const &path)
+{
+	return std::shared_ptr<widget::MemoryMapFile>{new widget::MemoryMapFile{path}};
+}
