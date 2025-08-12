c3c5e463ee2f030e2c58fd40e51629a99f8fbd02
hjc2000 <hjc.666@foxmail.com>
2025-08-12 10:05:02 +0800

    将Button的ConnectSignal方法实现从头文件移动到源文件

--- include/widget/button/Button.cpp
+++ include/widget/button/Button.cpp
@@ -1,1 +1,33 @@
 #include "Button.h" // IWYU pragma: keep
+
+void widget::Button::ConnectSignal()
+{
+	QMetaObject::Connection connection;
+
+	connection = connect(this,
+						 &QPushButton::clicked,
+						 [this]()
+						 {
+							 OnClicked();
+						 });
+
+	_connections.push_back(connection);
+
+	connection = connect(this,
+						 &QPushButton::pressed,
+						 [this]()
+						 {
+							 OnPressed();
+						 });
+
+	_connections.push_back(connection);
+
+	connection = connect(this,
+						 &QPushButton::released,
+						 [this]()
+						 {
+							 OnReleased();
+						 });
+
+	_connections.push_back(connection);
+}
--- include/widget/button/Button.h
+++ include/widget/button/Button.h
@@ -143,37 +143,7 @@ namespace widget
 
 		/* #endregion */
 
-		void ConnectSignal()
-		{
-			QMetaObject::Connection connection;
-
-			connection = connect(this,
-								 &QPushButton::clicked,
-								 [this]()
-								 {
-									 OnClicked();
-								 });
-
-			_connections.push_back(connection);
-
-			connection = connect(this,
-								 &QPushButton::pressed,
-								 [this]()
-								 {
-									 OnPressed();
-								 });
-
-			_connections.push_back(connection);
-
-			connection = connect(this,
-								 &QPushButton::released,
-								 [this]()
-								 {
-									 OnReleased();
-								 });
-
-			_connections.push_back(connection);
-		}
+		void ConnectSignal();
 
 	public:
 		/* #region 构造函数 */
