efd0e201b18a854efa692069ada28491770a3a14
hjc2000 <hjc.666@foxmail.com>
2025-08-13 09:31:15 +0800

    为DateTimeEdit添加时间点获取和提交事件支持，优化成员变量命名

--- include/widget/line-input-widget/DateTimeEdit.h
+++ include/widget/line-input-widget/DateTimeEdit.h
@@ -1,5 +1,7 @@
 #pragma once
+#include "base/delegate/Delegate.h"
 #include "base/IDisposable.h"
+#include "base/time/TimePointSinceEpoch.h"
 #include "qdatetimeedit.h"
 #include "qwidget.h"
 #include "widget/layout/HBoxLayout.h"
@@ -13,9 +15,16 @@ namespace widget
 	private:
 		bool _disposed = false;
 		widget::HBoxLayout _layout{this};
-		std::shared_ptr<QDateTimeEdit> _line_edit{new QDateTimeEdit{}};
+		std::shared_ptr<QDateTimeEdit> _edit{new QDateTimeEdit{}};
+
+		base::Delegate<> _submit_event;
 
 	public:
+		~DateTimeEdit()
+		{
+			Dispose();
+		}
+
 		///
 		/// @brief 处置对象，让对象准备好结束生命周期。类似于进入 “准备后事” 的状态。
 		///
@@ -30,6 +39,29 @@ namespace widget
 			}
 
 			_disposed = true;
+
+			_submit_event.Dispose();
+		}
+
+		///
+		/// @brief 提交事件。
+		///
+		/// @return
+		///
+		base::IEvent<> &SubmitEvent()
+		{
+			return _submit_event;
+		}
+
+		///
+		/// @brief 日期时间输入框的时间点。
+		///
+		/// @return
+		///
+		base::TimePointSinceEpoch TimePoint() const
+		{
+			QDateTime selectedDateTime = _edit->dateTime();
+			return base::TimePointSinceEpoch{std::chrono::seconds{selectedDateTime.toSecsSinceEpoch()}};
 		}
 	};
 
